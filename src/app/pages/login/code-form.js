d882964b3188ccecbe8de7a990018d7daed1af71
Vasyl Romanchak <vasyl.romanchak@myheritage.com>
2019-11-12 09:18:01 +0200

    add player

--- src/app/pages/login/code-form.js
+++ src/app/pages/login/code-form.js
@@ -1,6 +1,15 @@
+import '@lottiefiles/lottie-player';
+
 const template = document.createElement('template');
 template.innerHTML = `
     <div class="code_form">
+        <lottie-player
+            autoplay
+            mode="normal"
+            src="https://assets3.lottiefiles.com/packages/lf20_UJNc2t.json"
+            style="width: 320px"
+        >
+        </lottie-player>
         <h1></h1>
     </div>
 `;
